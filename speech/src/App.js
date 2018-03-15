aefa03764b593971438425a4ff0b72d939726fd9
VIVIANA MARCELO <arivichi16@gmail.com>
2018-03-15 11:18:24 -0300

    singOut

--- speech/src/App.js
+++ speech/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import AccesoGoogle from './components/AccesoGoogle';
-import firebase from 'firebase';
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,7 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-       <AccesoGoogle onClick/>
+       <AccesoGoogle />
       </div>
     );
   }
--- speech/src/components/AccesoGoogle.js
+++ speech/src/components/AccesoGoogle.js
@@ -3,14 +3,19 @@ import firebase from 'firebase';
 
 
 class AccesoGoogle extends Component {
-    // constructor(){
-    //     super();
-    //     const usuario = this.state  = {
-    //        user: null,
-    //     }
-    // }
+    constructor(){
+        super();
+         this.state  = {
+           user: null,
+        }
+        this.inicio = this.inicio.bind(this);
+        this.logOut = this.logOut.bind(this);
+    }
 
-    componentDidMount(){
+    componentWillMount(){//se dispara cuando el componente fue renderizado
+      firebase.auth().onAuthStateChanged(user =>{
+          this.setState({ user })
+      })
     }
     inicio(){
        const provider = new firebase.auth.GoogleAuthProvider();
@@ -19,11 +24,32 @@ class AccesoGoogle extends Component {
          .then(results => console.log(`${results.user.email} ha iniciado sesion`))
         .catch(error => console.log(`error ${error.code}: ${error.message}`));    
         }
+    logOut(){
+        firebase.auth().signOut()
+         .then(results => console.log(`${results.user.email} ha icerrado sesion`))
+        .catch(error => console.log(`error ${error.code}: ${error.message}`)); 
+    }    
 
+        renderLoginButton(){
+            if (this.state.user) {
+                return (
+                    <div>
+                      <img width="200px" height="200px" src={this.state.user.photoURL} alt={this.state.user.displayName}/>
+                      <p>{this.state.user.displayName}</p>
+                       <button onClick={this.logOut}>Cerrar Sesion </button>
+                     </div>
+                )
+            }else{
+                return(
+                <button onClick={this.inicio}>inicia Sesion con google</button>
+                )
+            }
+        }
+    
     render() {
         return (
         <div className="App">
-           <button onClick={this.inicio}>inicia Sesion con google</button>
+           <p > {this.renderLoginButton()}</p>
           </div>
         );
       }
